0afed5f54c322f9a9856af3aa0ec22fc406c8919
Paul Kotlov <paulkotlov@gmail.com>
2021-01-05 20:12:46 +0100

    Set GPU growth = True, for TF 2.2.0

--- donkeycar/parts/keras.py
+++ donkeycar/parts/keras.py
@@ -26,7 +26,18 @@ from tensorflow.python.keras.layers import LSTM
 from tensorflow.python.keras.layers.wrappers import TimeDistributed as TD
 from tensorflow.python.keras.layers import Conv3D, MaxPooling3D, Cropping3D, Conv2DTranspose
 
-import donkeycar as dk
+if tf.__version__ == '2.2.0':
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
 
 if tf.__version__ == '1.13.1':
     from tensorflow import ConfigProto, Session
@@ -37,6 +48,8 @@ if tf.__version__ == '1.13.1':
     config.gpu_options.allow_growth = True
     session = Session(config=config)
     keras.backend.set_session(session)
+
+import donkeycar as dk
 
 
 class KerasPilot(object):
